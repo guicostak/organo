89dfb8a9c6e87ce67177ba8654b00f47b6875398
guicostak <103201399+guicostak@users.noreply.github.com>
2023-02-07 16:23:12 -0300

    criando formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Banner from './componentes/Banner'
+import Formulario from './componentes/Formulario'
 import CampoTexto from './componentes/CampoTexto'
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   )
 }

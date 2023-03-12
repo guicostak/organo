ab8a6e4377b6dc30a2355368c6c7beadde93fb07
guicostak <103201399+guicostak@users.noreply.github.com>
2023-03-12 10:08:21 -0300

    .

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,19 +1,18 @@
+
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
   const placeholderModificada = `${props.placeholder}...`
 
-  let valor = ''
-
   const aoDigitado = (evento) =>{
-    valor = evento.target.value
+    props.aoAlterado(evento.target.value)
   }
 
   return(
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+      <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
     </div>
   )
 }
